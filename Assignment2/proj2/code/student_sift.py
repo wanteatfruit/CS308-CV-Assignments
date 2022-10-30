026bf834633b776d79d21eac2e7f2344005fd40d
Jesse An <feasiblechart@163.com>
2022-10-30 23:38:50 +0800

    added  sift

--- Assignment2/proj2/code/student_sift.py
+++ Assignment2/proj2/code/student_sift.py
@@ -65,10 +65,92 @@ def get_features(image, x, y, feature_width, scales=None):
     # decrease your matching accuracy.                                          #
     #############################################################################
 
-    raise NotImplementedError('`get_features` function in ' +
-        '`student_sift.py` needs to be implemented')
-
+    num_bins = 36
+    key_points = list(zip(x,y))
+    for p in key_points:
+        
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
     return fv
+    # Swap the coordinate to work with nupy array
+    x, y = y, x
+
+    size = int(feature_width//4)
+
+    # Calculate the Ix, Iy gradient vector
+    Ix = cv2.Sobel(image, cv2.CV_32F, 1, 0, ksize=5)
+    Iy = cv2.Sobel(image, cv2.CV_32F, 0, 1, ksize=5)
+
+    # Calculate the descriptors
+    descriptors = np.zeros((x.shape[0], 128))
+
+    for i, (kx, ky) in enumerate(zip(x, y)):
+        descriptors[i] = single_128_feature_vector(
+            Ix, Iy, int(kx), int(ky), size)
+
+    return descriptors
+
+    #############################################################################
+    #                             END OF YOUR CODE                              #
+    #############################################################################
+
+
+def single_128_feature_vector(Ix, Iy, kx, ky, size):
+    descriptor = []
+    for ix in range(-2, 2):
+        for iy in range(-2, 2):
+            locx, locy = kx + ix*size, ky + iy*size
+            histogram8 = histogram8bins(Ix, Iy, int(locx), int(locy), size)
+            descriptor.append(histogram8)
+
+    descriptor = np.array(descriptor).flatten()
+
+    #Normalize the descriptor
+    descriptor = descriptor / np.linalg.norm(descriptor)
+    descriptor = np.clip(descriptor, 0, 0.2)
+    descriptor = descriptor / np.linalg.norm(descriptor)
+
+    return descriptor
+
+
+def histogram8bins(Ix, Iy, indx, indy, size):
+    '''
+        Given a window of image (square of: size x size)
+        Return the total value of 8 bin histogram [0,45,90,135,180,225,270,325]
+    '''
+    histogram8 = np.zeros(8)
+    for x in range(size):
+        for y in range(size):
+            histogram8 = histogram8 + \
+                pixel2histogram(Ix[indx+x, indy+y], Iy[indx+x, indy+y])
+
+    return list(histogram8)
+
+
+def pixel2histogram(ix, iy):
+    '''
+        Given a gradient of a pixel in Cart form (ix, iy), convert to 8 bin histogram [0,45,90,135,180,225,270,325]
+        Input:
+            ix, iy: gradient at a given pixel
+        Return:
+            Histogram of this new pixel: array same length as [0,45,90,135,180,225,270,325]
+    '''
+    # Convert to polar
+    mag = np.sqrt(ix**2 + iy**2)
+    phase = np.arctan2(iy, ix)*180/np.pi
+
+    # convert from -180 --> 180 to 0 to 360
+    phase = phase + 360 if phase < 0 else phase
+    left = int(phase//45)
+    right = int(phase//45 + 1)  # if phase>325, right = 360
+
+    # padding 360 deg at the end to transform to [...,225,270,325,360]
+    pixel_histogram = np.zeros(9)
+    # Separate the vector into 2 nearby angles
+    pixel_histogram[right] = mag*(phase-45*left)/45
+    pixel_histogram[left] = mag*(45*right-phase)/45
+    # If the right = 8 or phase = 360, then it is equivalent to add to 0
+    pixel_histogram[0] += pixel_histogram[8]
+
+    return pixel_histogram[:8]
